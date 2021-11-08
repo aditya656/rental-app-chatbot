5e07991233af52f107fc36f2b194062717783aa5
aditya656 <68677462+aditya656@users.noreply.github.com>
2021-11-08 18:49:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def response():
     res = 'Sorry, I cant understand that 😕'
     options = ['Thanks!\nHere is the list of previous orders you have placed.Type Any one order number to get details\n\nID:2736 Camera($45*3Days)\nID:2785 Bike($50*7Days)SunGlasses($2*7Days),'
     ,'Thanks!2','Thanks!3','Thanks!4']
-    if 'Hi' in query or 'Hello' in query:
+    if 'Hi' in query or 'hi' in query:
         res = 'Hello!!!\n\nWhat can I help you with?\n1.Previous Orders\n2.Tracking Order\n3.Complaints\n4.Call Customer Support'
         return jsonify({"response" : res})
     else:
         try:
             int(query)
-            return jsonify({"response" : options[int(query)]})
+            return jsonify({"response" : options[int(query)-1]})
         except:
             return jsonify({"response" : res})
 if __name__=="__main__":
